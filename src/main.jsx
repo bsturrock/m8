0f214dbe99cfed1e31c4a0d75515a9f99e06ae48
Brandon Sturrock <brandon.sturrock@outlook.com>
2024-01-25 13:43:03 -0500

    structure

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 import { BrowserRouter } from 'react-router-dom'
-import './fonts/norwester.otf'
+import '/static/fonts/norwester.otf'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
